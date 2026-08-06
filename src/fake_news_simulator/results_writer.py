e9b8e53def84fc3bc418f324706ecbcec051d606
ro5784he <ross.herberigs@gmail.com>
2026-08-06 23:11:58 +0200

    feat: csv for all generated scenarios

--- src/fake_news_simulator/results_writer.py
+++ src/fake_news_simulator/results_writer.py
@@ -3,14 +3,16 @@ from datetime import datetime
 from pathlib import Path
 
 from fake_news_simulator.results_summarizer import SpreadOverStepsSummary, ScenarioSummary
+from fake_news_simulator.scenario_generator import Scenario
 from fake_news_simulator.simulation import SimulationResult
 
 
 class ResultsWriter:
-    def write(self, experiment_name: str, simulation_results: list[SimulationResult],
+    def write(self, experiment_name: str, scenarios: list[Scenario], simulation_results: list[SimulationResult],
               scenario_summaries: list[ScenarioSummary], spread_summaries: list[SpreadOverStepsSummary]) -> Path:
 
         result_directory = self._create_result_directory(experiment_name)
+        self._write_scenario_table(result_directory, scenarios)
         self._write_all_simulation_results(result_directory, simulation_results)
         self._write_scenario_summaries(result_directory, scenario_summaries)
         self._write_spread_summaries(result_directory, spread_summaries)
@@ -24,6 +26,32 @@ class ResultsWriter:
         result_directory.mkdir(parents=True)
 
         return result_directory
+
+    @staticmethod
+    def _write_scenario_table(result_directory: Path, scenarios: list[Scenario]) -> None:
+        path = result_directory / "scenario_table.csv"
+
+        with path.open("w", encoding="utf-8", newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=["scenario_id", "number_of_nodes", "influencer_ratio",
+                                                      "share_probability", "recipient_ratio", "check_probability",
+                                                      "moderation_type", "moderation_threshold",
+                                                      "moderation_label_reduction_factor",
+                                                      "moderation_downrank_reduction_factor", "runs_per_scenario",
+                                                      "max_steps_per_run"])
+            writer.writeheader()
+
+            for scenario in scenarios:
+                writer.writerow({"scenario_id": scenario.scenario_id, "number_of_nodes": scenario.number_of_nodes,
+                                 "influencer_ratio": scenario.influencer_ratio,
+                                 "share_probability": scenario.share_probability,
+                                 "recipient_ratio": scenario.recipient_ratio,
+                                 "check_probability": scenario.check_probability,
+                                 "moderation_type": scenario.moderation_type.value,
+                                 "moderation_threshold": scenario.moderation_threshold,
+                                 "moderation_label_reduction_factor": scenario.moderation_label_reduction_factor,
+                                 "moderation_downrank_reduction_factor": scenario.moderation_downrank_reduction_factor,
+                                 "runs_per_scenario": scenario.runs_per_scenario,
+                                 "max_steps_per_run": scenario.max_steps_per_run})
 
     @staticmethod
     def _write_all_simulation_results(result_directory: Path, simulation_results: list[SimulationResult]) -> None:
