b043dc00269fc752974a56afddf89ba2b21e8b4a
ro5784he <ross.herberigs@gmail.com>
2026-08-06 22:50:40 +0200

    feat: progress indicators

--- src/fake_news_simulator/experiment_runner.py
+++ src/fake_news_simulator/experiment_runner.py
@@ -6,7 +6,9 @@ from fake_news_simulator.simulation import Simulation, SimulationResult
 
 
 class ExperimentRunner:
-    def run(self, scenarios: list[Scenario]) -> list[SimulationResult]:
+
+    @staticmethod
+    def run(scenarios: list[Scenario]) -> list[SimulationResult]:
         network_factory = NetworkFactory()
         simulation = Simulation()
         results = []
@@ -15,7 +17,12 @@ class ExperimentRunner:
         influencer_ratio = scenarios[0].influencer_ratio
         runs_per_scenario = scenarios[0].runs_per_scenario
 
+        total_simulations = len(scenarios) * runs_per_scenario
+        completed_simulations = 0
+
         for run_index in range(1, runs_per_scenario + 1):
+            print(f"Creating graph for run {run_index}/{runs_per_scenario}")
+
             base_graph = network_factory.create_graph(number_of_nodes, influencer_ratio)
             start_node = int(numpy.random.choice(list(base_graph.nodes)))
 
@@ -23,5 +30,8 @@ class ExperimentRunner:
                 base_graph_copy = base_graph.copy()
                 scenario_result = simulation.run(scenario, run_index, base_graph_copy, start_node)
                 results.append(scenario_result)
+
+                completed_simulations += 1
+                print(f"Completed simulation {completed_simulations}/{total_simulations}")
 
         return results
